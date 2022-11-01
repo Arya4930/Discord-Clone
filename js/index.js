1de624ca975ee2d85c7383375a490e8f12e8f7c4
arya4930 <aryapanwar493@gmail.com>
2022-11-01 21:56:55 +0530

    adding stuff

--- js/index.js
+++ js/index.js
@@ -1,3 +1,24 @@
-const DiscordLinkButton = () => {
-    window.location.href = "home.html"
+function DiscordLinkButton() {
+    location.assign("loader.html");
+}
+const DownloadLinkButton = () => {
+    window.location.href = "https://discord.com/download"
+}
+const NitroLinkButton = () => {
+    window.location.href = "https://discord.com/nitro"
+}
+const DiscoverLinkButton = () => {
+    window.location.href = "https://discord.com/servers"
+}
+const SafetyLinkButton = () => {
+    window.location.href = "https://discord.com/safetycenter"
+}
+const SupportLinkButton = () => {
+    window.location.href = "https://support.discord.com/hc/en-us"
+}
+const blogLinkButton = () => {
+    window.location.href = "https://discord.com/blog"
+}
+const CarrersLinkButton = () => {
+    window.location.href = "https://discord.com/careers"
 }
