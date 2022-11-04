a2a309db32ec319f9356c7ed96d4a7fd9cbe473d
arya4930 <aryapanwar493@gmail.com>
2022-11-04 21:15:53 +0530

    Home-commit-3

--- js/index.js
+++ js/index.js
@@ -1,24 +1,19 @@
-function DiscordLinkButton() {
-    location.assign("loader.html");
-}
-const DownloadLinkButton = () => {
-    window.location.href = "https://discord.com/download"
-}
-const NitroLinkButton = () => {
-    window.location.href = "https://discord.com/nitro"
-}
-const DiscoverLinkButton = () => {
-    window.location.href = "https://discord.com/servers"
-}
-const SafetyLinkButton = () => {
-    window.location.href = "https://discord.com/safetycenter"
-}
-const SupportLinkButton = () => {
-    window.location.href = "https://support.discord.com/hc/en-us"
-}
-const blogLinkButton = () => {
-    window.location.href = "https://discord.com/blog"
-}
-const CarrersLinkButton = () => {
-    window.location.href = "https://discord.com/careers"
-}
+const DiscordLinkButton = () =>location.assign("loader.html");
+
+const DownloadLinkButton = () => window.location.href = "https://discord.com/download"
+
+const NitroLinkButton = () => window.location.href = "https://discord.com/nitro"
+
+const DiscoverLinkButton = () => window.location.href = "https://discord.com/servers"
+
+const SafetyLinkButton = () => window.location.href = "https://discord.com/safetycenter"
+
+const SupportLinkButton = () => window.location.href = "https://support.discord.com/hc/en-us"
+
+const blogLinkButton = () => window.location.href = "https://discord.com/blog"
+
+const CarrersLinkButton = () => window.location.href = "https://discord.com/careers"
+
+
+
+const Discordlogobutton = () => window.location.href = "home.html"
